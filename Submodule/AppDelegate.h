5755f7fd2078f37fcc856c48f34f70a24c005187
FarTeen <934628017@qq.com>
2015-10-26 14:02:33 +0800

    init commit

---
+++ Submodule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Submodule
+//
+//  Created by user on 10/26/15.
+//  Copyright © 2015 FarTeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Submodule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Submodule
+//
+//  Created by user on 10/26/15.
+//  Copyright © 2015 FarTeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
